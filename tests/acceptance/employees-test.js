87ad596e4df70adae36771a7931d14a6beccdc93
Sophia Pignatelli <pignatelli@fordham.edu>
2017-07-03 00:20:30 -0400

    add acceptance tests

--- tests/acceptance/employees-test.js
+++ tests/acceptance/employees-test.js
@@ -10,3 +10,19 @@ test('visiting /', function(assert) {
     assert.equal(currentURL(), '/');
   });
 });
+
+test('should link to information about company', function(assert) {
+	visit('/');
+	click('a:contains("About")');
+	andThen(function() {
+		assert.equal(currentURL(), '/about', 'should navigate to about');
+	});
+});
+
+test('should link to contact information', function(assert) {
+	visit('/');
+	click('a:contains("Contact")');
+	andThen(function() {
+		assert.equal(currentURL(), '/contact', 'should navigate to about');
+	});
+});
