637344e1f085ff2adf6d0e5ca38d3d72073d9d47
Sophia Pignatelli <pignatelli@fordham.edu>
2017-07-04 01:11:04 -0400

    edit

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,33 @@
 export default function() {
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+   // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  /*
-    Shorthand cheatsheet:
+  
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
+    this.get('/', function(){
+      return {
+        data: [{
+          id:'model-home-day',
+          title: 'Model Home in Newport Beach',
+          development: 'Sudden Valley'
+        }, {
+          id:'model-home-night',
+          title: 'Model Home in Newport Beach',
+          development: 'Sudden Valley'
+        },{
+          id:'model-home-iraq',
+          title: 'Model Home in Iraq',
+          development: 'Baghdad'
+        },{
+          id:'model-home-mexico',
+          title: 'Model Home in Newport Beach',
+          development: 'Baja California'
+        },]
+      }
+    });
+   
 
-    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
-  */
+  
 }
