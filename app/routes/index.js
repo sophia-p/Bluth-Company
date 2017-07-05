ae6d4c80a2e0ed293b6f1f128142e7b61064cb95
Sophia Pignatelli <pignatelli@fordham.edu>
2017-07-04 23:26:08 -0400

    rename images and add to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
 	model() {
 		return [{
 					id:'model-home-day',
-					img: '/assets/modelhome1.png',
 					title: 'Model Home in Newport Beach',
 					development: 'Sudden Valley'
 				}, {
