0eba4a8bb027e339dff63571c86b308619477505
Sophia Pignatelli <pignatelli@fordham.edu>
2017-07-04 23:44:29 -0400

    add employee pos and id

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -2,6 +2,39 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(){
-		return ['George Bluth', 'Lucille Bluth', 'Michael Bluth', 'Kitty Sanchez', 'Tobias Fünke', 'Starla', 'Lindsay Bluth Fünke']
+		return [{
+			name: 'George Bluth',
+			position: 'Founder',
+			id: 'george-bluth'}, 
+			{
+			name: 'Lucille Bluth',
+			position: 'CEO',
+			id: 'lucille-bluth'
+			},
+			{
+			name: 'Michael Bluth',
+			position: 'President',
+			id: 'michael-bluth',
+			},
+			{
+			name: 'Kitty Sanchez',
+			position: 'Personal Assistant',
+			id: 'kitty-sanchez' 
+			},
+			{
+			name: 'Tobias Fünke',
+			position: 'Personal Assistant',
+			id: 'tobias-funke'
+			},
+			{
+			name: 'Starla',
+			position: 'Business Model',
+			id: 'starla'
+			},
+			{
+			name: 'Lindsay Bluth Fünke',
+			position: 'Secretary',
+			id: 'lindsay-funke'
+			}]
 	}
 });
