a0f30c0bbca310977189c36e6665c88664e5df97
Sophia Pignatelli <pignatelli@fordham.edu>
2017-07-09 23:14:51 -0400

    edit test

--- tests/integration/components/model-home-test.js
+++ tests/integration/components/model-home-test.js
@@ -16,8 +16,8 @@ moduleForComponent('model-home', 'Integration | Component | model home', {
 test('it should display model home details', function(assert) {
   this.set('modelHomeObj', modelHome );
   this.render(hbs`{{model-home modelHome=modelHomeObj}}`);
-  assert.equal(this.$('.listing h3').text(), 'test-title');
-  assert.equal(this.$('.listing .development').text().trim(), 'home description');
+  assert.equal(this.$('article.listing h3').text(), 'test-title');
+  assert.equal(this.$('.listing div.development').text().trim(), 'home description');
 });
 
 test('it should toggle wide class on click', function(assert) {
